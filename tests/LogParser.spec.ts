6238ff8c7ddc937b3d0afd6a0883631f52b33034
Icaro Silva <ikaromarlon@gmail.com>
2023-09-05 14:22:09 +0200

    test: add validation to accept only Error logs

--- tests/LogParser.spec.ts
+++ tests/LogParser.spec.ts
@@ -16,20 +16,36 @@ describe('LogParser unit tests', () => {
   test('Should parse log line successfully', () => {
     const { sut } = makeSut()
 
-    const result = sut.parse('2044-08-09T02:12:51.253Z - info - {"transactionId":"9abc55b2-807b-4361-9dbe-aa88b1b2e978","details":"Service is started"}')
+    const logLine = '2021-08-09T02:12:51.259Z - error - {"transactionId":"9abc55b2-807b-4361-9dbe-aa88b1b2e978","details":"Cannot find user orders list","code": 404,"err":"Not found"}'
+
+    const result = sut.parse(logLine)
 
     expect(result).toEqual({
-      timestamp: 2354321571253,
-      loglevel: 'info',
+      timestamp: 1628475171259,
+      loglevel: 'error',
       transactionId: '9abc55b2-807b-4361-9dbe-aa88b1b2e978',
-      details: 'Service is started'
+      err: 'Not found',
+      code: 404,
+      details: 'Cannot find user orders list'
     })
+  })
+
+  test('Should not parse log line due to unexpected loglevel', () => {
+    const { sut } = makeSut()
+
+    const logLine = '2044-08-09T02:12:51.253Z - info - {"transactionId":"9abc55b2-807b-4361-9dbe-aa88b1b2e978","details":"Service is started"}'
+
+    const result = sut.parse(logLine)
+
+    expect(result).toEqual(null)
   })
 
   test('Should not parse log line due to invalid format', () => {
     const { sut } = makeSut()
 
-    const result = sut.parse('2044-08-09T02:12:51.253Z info {"transactionId":"9abc55b2-807b-4361-9dbe-aa88b1b2e978","details":"Service is started"}')
+    const logLine = '2044-08-09T02:12:51.253Z info {"transactionId":"9abc55b2-807b-4361-9dbe-aa88b1b2e978","details":"Service is started"}'
+
+    const result = sut.parse(logLine)
 
     expect(result).toEqual(null)
   })
