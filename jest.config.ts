7afbcf08248e95e27f22e42057fbc974df9b2a9a
Icaro Silva <ikaromarlon@gmail.com>
2023-11-02 17:39:30 +0100

    chore: change tests coverage

--- jest.config.ts
+++ jest.config.ts
@@ -23,8 +23,7 @@ const config: Config = {
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
-    'src/*.ts',
-    '!src/parser.ts'
+    'src/*.ts'
   ],
 
   // The directory where Jest should output its coverage files
